75fd4247ea79f853d16f4004a2e42607158c4046
Mark Leb <wscad@yandex.ru>
2021-02-27 11:26:29 +0300

    add nodemon

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const config = require('config');
 
 const PORT = config.get('serverPort');
 
+app.use(express.static('./public'));
+
+//START SERVER
+const start = () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`LAUNCH SERVER! on port ${PORT}`);
+        })
+    } catch(e) {
+
+    }
+}
+start();
 
 
 
@@ -13,8 +26,3 @@ const PORT = config.get('serverPort');
 
 
 
-
-
-app.listen(PORT, () => {
-    console.log(`LAUNCH SERVER! on port ${PORT}`);
-})
