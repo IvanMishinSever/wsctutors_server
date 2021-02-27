feb6cad3c2b7372b28fafacdc9cd57413cfcb657
Mark Leb <wscad@yandex.ru>
2021-02-27 10:00:37 +0300

    add server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 4001;
+
+
+
+
+
+
+
+
+
+
+app.listen = (PORT, () => {
+    console.log(`LAUNCH SERVER! on port ${PORT}`);
+}) 
